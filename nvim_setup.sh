dd2063ee64779f0aac50c0dbc4c7c724c38677a8
ns <enesdindas26@gmail.com>
2021-11-01 11:12:30 +0300

    nvim setup updated

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+brew install nvim
+brew upgrade nvim
 rm -rf ~/.config/nvim
 cp -r nvim ~/.config
+nvim +PlugInstall +qall
+echo "You can edit your Language Server Protocols from ~/.config/nvim/after/plugin/lspconfig.rc.vim."
+echo "You need to install requirements of your Language Server Protocol configs."
+echo "Please see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md for requirements of your configs"
