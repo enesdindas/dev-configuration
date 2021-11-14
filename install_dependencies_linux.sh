b6eb3e551779884fd6dcffd767c9bde403a4213b
ns <enesdindas26@gmail.com>
2021-11-15 00:55:18 +0300

    linux fix

--- install_dependencies_linux.sh
+++ install_dependencies_linux.sh
@@ -1,13 +1,9 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 sudo apt install rbenv
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
-
-# Optional: exposing nvim globally
-mv squashfs-root / && ln -s /squashfs-root/AppRun /usr/bin/nvim
+~/.fzf/install -y
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update -y
+sudo apt install neovim -y
 mkdir ~/.config
-cp nvim ~/.config/nvim
+cp -r nvim ~/.config/nvim
