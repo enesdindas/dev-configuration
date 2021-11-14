0ddf8b246d48a2d80f77770a2cce45bc45939292
ns <enesdindas26@gmail.com>
2021-11-15 00:39:10 +0300

    neovim installation

--- install_dependencies_linux.sh
+++ install_dependencies_linux.sh
@@ -2,4 +2,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 sudo apt install rbenv
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-
+sudo apt install neovim
+mkdir ~/.config
+cp nvim ~/.config/nvim
--- install_dependencies_macos.sh
+++ install_dependencies_macos.sh
@@ -1,4 +1,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 brew install rbenv
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+brew install neovim/.fzf/install
+mkdir ~/.config
+cp nvim ~/.config/nvim
