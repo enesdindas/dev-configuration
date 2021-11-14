1aa116764c0bfac1bf49657187948b1f45e40f5f
ns <enesdindas26@gmail.com>
2021-11-15 00:35:50 +0300

    zshrc and dependencies(node, ruby and fzf)

---
+++ install_dependencies_linux.sh
@@ -0,0 +1,5 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+sudo apt install rbenv
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
---
+++ install_dependencies_macos.sh
@@ -0,0 +1,4 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+brew install rbenv
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
