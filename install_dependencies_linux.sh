28af95715f05cf3bc3922c9fe833bf45e6868002
ns <enesdindas26@gmail.com>
2021-11-15 01:06:16 +0300

    missing parts

--- install_dependencies_linux.sh
+++ install_dependencies_linux.sh
@@ -1,9 +1,12 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 sudo apt install rbenv
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install -y
+~/.fzf/install
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update -y
 sudo apt install neovim -y
+pip3 install --user pynvim
 mkdir ~/.config
 cp -r nvim ~/.config/nvim
--- install_dependencies_macos.sh
+++ install_dependencies_macos.sh
@@ -2,5 +2,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 brew install rbenv
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 brew install neovim/.fzf/install
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pip3 install --user pynvim
 mkdir ~/.config
-cp nvim ~/.config/nvim
+cp -r nvim ~/.config/nvim
