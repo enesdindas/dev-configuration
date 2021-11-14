7e67d512189313bd46f1e627e765567b0638c0b6
ns <enesdindas26@gmail.com>
2021-11-15 00:48:03 +0300

    neovim linux installation

--- install_dependencies_linux.sh
+++ install_dependencies_linux.sh
@@ -2,6 +2,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 sudo apt install rbenv
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-sudo apt install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally
+mv squashfs-root / && ln -s /squashfs-root/AppRun /usr/bin/nvim
 mkdir ~/.config
 cp nvim ~/.config/nvim
