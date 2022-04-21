dac2165ca591639869beb1f8db58178ac42b9f9c
ns <enesdindas26@gmail.com>
2022-04-21 15:18:24 +0300

    auto start sorbet and remove pakcege.yml

--- nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/lua/plugins/configs/lspconfig.lua
@@ -47,10 +47,10 @@ local setup_lsp = function(attach)
         cmd = {"solargraph", "stdio"},
         init_options = {formatting = false},
         settings = {solargraph = {diagnostics = false}},
-        root_dir = lspconfig.util.root_pattern('Gemfile', '.git')
+        root_dir = lspconfig.util.root_pattern('Gemfile', '.git', 'package.yml')
     }
 
-    lspconfig.sorbet.setup {on_attach = attach, capabilities = capabilities, autostart = false}
+    lspconfig.sorbet.setup {on_attach = attach, capabilities = capabilities}
 
     lspconfig.rls.setup {
         on_attach = attach,
